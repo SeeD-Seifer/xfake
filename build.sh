65b6b6441a5fc0d8352a888eea7a879859858b6e
Joe Cooper <joe@artofbytes.com>
2017-09-07 13:49:48 +0200

    Amended to depend on FAKE 4.63.0 instead of 4.0.3

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nuget install FAKE -Version 4.0.3 -o packages
-mono --runtime=v4.0 packages/FAKE.4.0.3/tools/FAKE.exe xfake/build.fsx $@
+nuget install FAKE -Version 4.63.0 -o packages
+mono --runtime=v4.0 packages/FAKE.4.63.0/tools/FAKE.exe xfake/build.fsx $@
